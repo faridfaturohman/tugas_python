69c8f1c41beda2263e29e28db95ccf6d65d55328
faridfaturohman <37410731+faridfaturohman@users.noreply.github.com>
2018-03-15 02:51:26 -1200

    tugas python

---
+++ tugas py 2.py
@@ -0,0 +1,24 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: D:\texttable-1.2.1\tugas py 1.py =================
+Masukan Nama :ringgo
+Masukan Nim  :1234
+Nilai Tugas  :90
+Nilai UTS    :90
+Nilai UAS    :90
+Tambah data (y/t)?y
+Masukan Nama :rangga
+Masukan Nim  :2345
+Nilai Tugas  :95
+Nilai UTS    :95
+Nilai UAS    :95
+Tambah data (y/t)?t
++----+--------+------+-------+-----+-----+-------+
+| No |  Nama  | NIM  | TUGAS | UTS | UAS | AKHIR |
++====+========+======+=======+=====+=====+=======+
+| 1  | ringgo | 1234 | 90    | 90  | 90  | 90    |
++----+--------+------+-------+-----+-----+-------+
+| 2  | rangga | 2345 | 95    | 95  | 95  | 95    |
++----+--------+------+-------+-----+-----+-------+
+>>> 
